e548dc1e37ae0590473753707fa014a7f6cc8986
Jordan Smith <jordan0smith7@gmail.com>
2024-03-01 11:16:04 -0600

    Update blocks to include point value

--- blocks.py
+++ blocks.py
@@ -8,12 +8,14 @@ class Blocks(Turtle):
         super().__init__()
         self.x = START_X
         self.y = 50
+        self.point_value = 0
         self.blocks = []
         self.create_blocks()
 
     def create_blocks(self):
         for color in ROW_COLORS:
             self.x = START_X
+            self.point_value += 1 * 10
             self.create_row(color)
             self.y += 25
 
@@ -21,6 +23,7 @@ class Blocks(Turtle):
         for _ in range(12):
             new_block = Turtle('square')
             new_block.color(color)
+            new_block.point_value = self.point_value
             new_block.shapesize(stretch_wid=1, stretch_len=3)
             new_block.penup()
             new_block.goto(self.x, self.y)
