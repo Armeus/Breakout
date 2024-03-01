c893c062dd23646bdfee6af19afee9c6fc6a743b
Jordan Smith <jordan0smith7@gmail.com>
2024-03-01 11:16:23 -0600

    Add scoreboard functionality

---
+++ scoreboard.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+from blocks import Blocks
+
+ALIGNMENT = 'center'
+FONT = ('Courier', 30, 'normal')
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('white')
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        self.lives = 5
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(0, 250)
+        self.write(self.score, align=ALIGNMENT, font=FONT)
+        self.goto(-300, 250)
+        self.write(f'Lives={self.lives}', align=ALIGNMENT, font=FONT)
+
+    def increase_score(self, block):
+        self.score += block.point_value
+        self.update_scoreboard()
+
+    def decrease_lives(self):
+        self.lives -= 1
+        self.update_scoreboard()
+
